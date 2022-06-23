3aa527ef3c1828c835f7b0a5aeb90787ce2b5003
DerEchteFeuerpfeil <57487741+DerEchteFeuerpfeil@users.noreply.github.com>
2022-06-23 16:21:41 +0200

    changes paths to be relative

--- fastAPI/util.py
+++ fastAPI/util.py
@@ -2,14 +2,10 @@ import pandas as pd
 import numpy as np
 import json
 
-def get_dataframe(path = "G:\\GitHub\\onetask\\datalift-summit\\04_ModelPipeline\\labeled_data_v1.json"):
+def get_dataframe(path = "../04_ModelPipeline/output.csv"):
     # load the data
-    with open(path, "r") as f:
-        data = json.load(f)
-    df = pd.DataFrame(data)
-    df["merged_texts"] = df["headline"] + ". "+ df["body"]
-    return df
+    return pd.read_csv(path, quoting=1)
 
-def read_embeddings(path = "G:\\GitHub\onetask\\datalift-summit\\04_ModelPipeline\\embeddings.npy"):
+def read_embeddings(path = "../04_ModelPipeline/embeddings.npy"):
     embeddings = np.load(path)
     return embeddings
